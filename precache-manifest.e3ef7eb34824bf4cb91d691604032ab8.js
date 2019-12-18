1ff36dedc6d9bd7c26e795c1ffb9f81e79ba0b1d
VitaliiKulyk <vkul@eliftech.com>
2019-12-18 03:23:31 +0200

    Updates

---
+++ precache-manifest.e3ef7eb34824bf4cb91d691604032ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4f3ed1b5bd07e3b3e81b5543a7a5d4",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "94af9dbc8ba9392ea52a",
+    "url": "/test-app/static/css/main.43073337.chunk.css"
+  },
+  {
+    "revision": "03b475c45260b9ca33c5",
+    "url": "/test-app/static/js/2.cb169a95.chunk.js"
+  },
+  {
+    "revision": "49309882d0acd110106c7be3de4100a8",
+    "url": "/test-app/static/js/2.cb169a95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94af9dbc8ba9392ea52a",
+    "url": "/test-app/static/js/main.824de6a1.chunk.js"
+  },
+  {
+    "revision": "bee113f8d0a2cac5ae22",
+    "url": "/test-app/static/js/runtime-main.7aa8fb11.js"
+  }
+]);
