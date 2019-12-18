8db40ac2dbd3b7710686ef702c540e31bb9e6c21
VitaliiKulyk <vkul@eliftech.com>
2019-12-18 03:12:48 +0200

    deployer docs folder

---
+++ docs/precache-manifest.1c742af8e91bc6633112f9f37ac11868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79aff74c2c0256f995283dab237b602f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94af9dbc8ba9392ea52a",
+    "url": "/static/css/main.43073337.chunk.css"
+  },
+  {
+    "revision": "03b475c45260b9ca33c5",
+    "url": "/static/js/2.cb169a95.chunk.js"
+  },
+  {
+    "revision": "49309882d0acd110106c7be3de4100a8",
+    "url": "/static/js/2.cb169a95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94af9dbc8ba9392ea52a",
+    "url": "/static/js/main.824de6a1.chunk.js"
+  },
+  {
+    "revision": "f800faec70460eb60f00",
+    "url": "/static/js/runtime-main.c1b96ac7.js"
+  }
+]);
